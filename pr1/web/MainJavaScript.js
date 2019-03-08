0de1fd7c3617b86b8ec501330d94458afa6bc8b5
Toni <tboutaour@gmail.com>
2019-03-08 18:41:39 +0100

    Nueva feature a medias. Implementación de botones por personaje

--- pr1/web/MainJavaScript.js
+++ pr1/web/MainJavaScript.js
@@ -3,6 +3,28 @@ var personatges =["Brian_Griffin","Chris_Griffin","Cleveland_Brown","Glenn_Quagm
 
 var myVideo = document.getElementById("mainVid");
 
+var scene_track = myVideo.textTracks[2];
 $(function () {
-    start_track ()
+    start_track ();
 })
+
+
+function click_personatge(personatge) {
+    var max;
+    var min;
+    var rnd;
+    min = 0;
+    max= scene_track.cues.length-1;
+    rnd = Math.round(Math.random() * (max - min) + min);
+    console.log(rnd.toString());
+    var info = JSON.parse(scene_track.cues[rnd].text);
+
+    if (info.characters.includes(personatge)){
+/*        myVideo.play();
+        myVideo.currentTime=scene_track.cues[rnd].startTime;
+        console.log(myVideo.currentTime);*/
+        return;
+    }else{
+        click_personatge(personatge)
+    }
+}
--- pr1/web/tracks.js
+++ pr1/web/tracks.js
@@ -1,6 +1,6 @@
 function start_track () {
     myVideo.addEventListener('loadeddata', function() {
-        var scene_track = myVideo.textTracks[2];
+
         scene_track.mode = 'showing';
         // Cada vez que se cambie el cue: Actualizamos panel de informacion de escena (t characters y screenshots)
         scene_track.oncuechange = function(e) {
@@ -11,11 +11,13 @@ function start_track () {
                 personatge = document.getElementById(personatges[s]);
                 personatge.style.opacity="0.2";
             }
+
             var activeCue = scene_track.activeCues[0];
             if (activeCue) {
                 var info = JSON.parse(activeCue.text);
                 for (i in info.characters){
                     console.log(info.characters[i]);
+                    console.log(myVideo.currentTime);
                     element = document.getElementById(info.characters[i]);
                     element.style.opacity="0.9";
                 }
